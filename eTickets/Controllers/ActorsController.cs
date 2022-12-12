f4e0387942cac3ea9d2b5a7ff38ec7b8107af3aa
mahmoudklaled <mahmoud.khaled19640@gmail.com>
2022-12-12 15:05:14 +0200

    first update

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,33 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorsService _services;
+
+        public ActorsController(IActorsService services)
+        {
+            _services = services;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data =await _services.GetAll();
+            return View(data);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+
+            var data = await _services.GetAll();
+            return View();
+        }
+    }
+}
---
+++ eTickets/Data/Services/ActrorServices.cs
@@ -0,0 +1,44 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ActrorServices : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActrorServices(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Actor actor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+
+            var result = await _context.Actors.ToListAsync();
+            return result;
+            //throw new System.NotImplementedException();
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
